4a9b8130045ce42fecf205b0e8ed68d97157e89b
Yujing Zheng <hzyh64160@gmail.com>
2013-08-13 00:44:51 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Contacts::Application.config.secret_key_base = 'b95f5ba7e44072f9973584d551ba3a2f39f87d6434d77f721eb308d2dd90d02abe2950d20c04cd27a8a459e74e6f899ce494e78b4d6e021afea3faed072722fb'
